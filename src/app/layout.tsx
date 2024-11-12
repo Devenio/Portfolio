90a44bf38c8777de0c753d1dc85c1784563c1ad5
Mahziyar Erfani <mhzrerfani@gmail.com>
2024-11-12 09:11:55 +0330

    Add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import { FuzzyOverlay } from "@/components/ui/fuzzy-overlay";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import Cursor from "@/components/ui/cursor";
+import { GoogleTagManager } from "@next/third-parties/google";
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
@@ -14,8 +16,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      {GTM_ID && <GoogleTagManager gtmId={GTM_ID} />}
       <body className={`${roboto.variable} h-dvh`}>
-        <div className="h-full relative overflow-hidden">
+        <div className="h-full relative overflow-hidden overscroll-none">
           {children}
           <FuzzyOverlay />
           <Cursor />
