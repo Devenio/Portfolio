7fbf8683a904be75cc69dda76d52b76a78bac89c
Mahziyar Erfani <mhzrerfani@gmail.com>
2024-11-18 22:45:35 +0330

    Fix blog post metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,19 +9,24 @@ type BlogPostPageProps = {
     params: { slug: string };
 };
 
-export async function generateStaticParams() {
+export function generateStaticParams() {
     const posts = getAllPosts();
     return posts.map((post) => ({ slug: post.slug }));
 }
 
-export default async function BlogPostPage({ params }: BlogPostPageProps) {
+export default function BlogPostPage({ params }: BlogPostPageProps) {
     const post: PostContent = getPostBySlug(params.slug);
 
     return (
         <article>
             <div className="bg-theme-accent p-10 pb-5 rounded-b-2xl">
-                <Link href="/blog" className='mb-5 inline-block bg-theme-background border border-theme-accent fixed text-sm rounded-xl text-theme-primary py-2 px-3 font-semibold z-30'>Go to Posts</Link>
-                <h1 className="text-4xl font-bold text-theme-primary mb-4 pt-12 ">
+                <Link
+                    href="/blog"
+                    className="mb-5 inline-block bg-theme-background border border-theme-accent fixed text-sm rounded-xl text-theme-primary py-2 px-3 font-semibold z-30"
+                >
+                    Go to Posts
+                </Link>
+                <h1 className="text-4xl font-bold text-theme-primary mb-4 pt-12">
                     {post.metadata.title}
                 </h1>
                 <p className="text-theme-secondary mb-6 font-bold">
@@ -38,23 +43,24 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
 export async function generateMetadata({
     params,
 }: BlogPostPageProps): Promise<Metadata> {
-    const post = getPostBySlug(params.slug);
+    const slug = params.slug;
+    const post = getPostBySlug(slug);
 
     const domain =
-        process.env.NEXT_PUBLIC_WEBSITE_DOMAIN || 'http://localhost:3000';
-    const url = `${domain}/blog/${post.metadata.slug}`;
-    const image = `${domain}${post.metadata.cover}`;
+        process.env.NEXT_PUBLIC_WEBSITE_DOMAIN;
+    const url = `${domain}/blog/${slug}`;
+    const image = `${domain}/blog/${post.metadata.cover}`;
 
     return {
         title: post.metadata.title,
         description: post.metadata.description,
-        keywords: post.metadata.keywords.join(', '),
         authors: [{ name: 'Mahziyar Erfani' }],
+        keywords: post.metadata.keywords.join(', '),
         openGraph: {
             title: post.metadata.title,
             description: post.metadata.description,
-            url,
             type: 'article',
+            url,
             publishedTime: post.metadata.date,
             images: [
                 {
