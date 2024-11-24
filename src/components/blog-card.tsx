69bb5ae9b79502094a5b167ca8b68767ee673e77
Mahziyar Erfani <mhzrerfani@gmail.com>
2024-11-24 14:56:07 +0330

    Fix scroll on laptops

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -1,26 +1,26 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import { PostMetadata } from "@/lib/types";
+import Link from "next/link";
 
-export interface Post {
- title: string;
- date: string;
- description: string;
- cover: string;
- timeToRead: string;
- slug: string;
-}
-
-export default function BlogCard({ post }: { post: Post }) {
- return (
-  <li>
-   <Link href={`/blog/${post.slug}`}>
-    <Image src={post.cover} alt={post.title} />
-    <h2>{post.title}</h2>
-    <p>{post.description}</p>
-    <small>
-     {post.date} • {post.timeToRead}
-    </small>
-   </Link>
-  </li>
- );
+export default function BlogCard({ post }: { post: PostMetadata }) {
+    return <Link
+        href={`/blog/${post.slug}`}
+        key={post.slug}
+        className="p-5 block rounded-2xl bg-theme-background border border-theme-accent shadow-xl hover:shadow-2xl duration-300 transition-all delay-0 hover:border-theme-secondary hover:translate-x-1"
+    >
+        <h2 className="text-2xl font-bold text-white">{post.title}</h2>
+        <small className="text-theme-secondary">
+            {typeof post.date === 'string' ? post.date : post.date.toDateString()}
+        </small>
+        <p className="text-theme-primary my-3">{post.description}</p>
+        <div className="flex gap-2 flex-wrap">
+            {post.keywords.map((keyword) => (
+                <span
+                    className="bg-theme-accent px-2 py-1 text-sm rounded-2xl text-theme-primary"
+                    key={keyword}
+                >
+                    {keyword}
+                </span>
+            ))}
+        </div>
+    </Link>
 }
